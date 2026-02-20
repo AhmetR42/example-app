d4eb6a443aeefeee12ebd8d58f9a7db3d2778f2b
Ahmet <ahmet.akbulut@rocva.nl.com>
2026-02-20 16:43:32 +0100

    Added planets route and Blade view with foreach loop

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/planets', function () {
+
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant.'
+        ],
+    ];
+
+    return view('planets', compact('planets'));
 });
